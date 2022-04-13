d32ecec2b09b973ea45d0bdecb6934811dc3f64a
iyo-bemoore <imadyoussoufi@gmail.com>
2022-04-13 21:15:29 +0200

    fix badge

--- mui.overrides/theme.js
+++ mui.overrides/theme.js
@@ -56,8 +56,9 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
     right: -8,
     top: 5,
-    border: `2px solid ${theme.palette.background.paper}`,
     padding: "0 4px",
+    minWidth: "16px",
+    height: "16px",
   },
 }));
 
